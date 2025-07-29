c2b748cd200f9c8b52786496463d0c59f4d1d7ef
MJImran <mjimran0141@gmail.com>
2025-07-29 18:12:47 +0100

    about-section

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 "use strict";
 
 console.log("running index.js");
+
+const tabLinks = document.querySelectorAll(".tabs");
+const tabContents = document.querySelectorAll(".tab-content");
+
+const tabLink = document.getElementsByClassName("tabs");
+const tabContent = document.getElementsByClassName("tab-content");
+
+function openTab(tabName) {
+  for (const link of tabLink) {
+    link.classList.remove("active-tab");
+  }
+  for (const content of tabContent) {
+    content.classList.remove("active-cont");
+  }
+  event.currentTarget.classList.add("active-tab");
+  document.getElementById(tabName).classList.add("active-cont");
+}
