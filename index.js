ead937437393c2b1fe7071a55c1c4b86a4415b37
MJImran <mjimran0141@gmail.com>
2025-09-27 15:22:11 +0100

    event delegation

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 "use strict";
 
-console.log("running index.js");
-
-const tabLinks = document.querySelectorAll(".tabs");
-const tabContents = document.querySelectorAll(".tab-content");
-
-const tabLink = document.getElementsByClassName("tabs");
-const tabContent = document.getElementsByClassName("tab-content");
-
-function openTab(tabName) {
-  for (const link of tabLink) {
-    link.classList.remove("active-tab");
-  }
-  for (const content of tabContent) {
-    content.classList.remove("active-cont");
-  }
-  event.currentTarget.classList.add("active-tab");
-  document.getElementById(tabName).classList.add("active-cont");
-}
+// Selecting elements
 
 const mobileNavBtn = document.querySelector(".mobile-nav-btn");
-
 const navBar = document.querySelector(".header-nav");
+const tabContainer = document.querySelector(".tab-container");
+const tabLinks = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
 
-const navOpen = () => {
+// Functions
+
+const mobileNavOpen = () => {
   navBar.classList.toggle("nav-open");
-  // mobileNavBtn.classList.toggle("nav-open");
 };
-mobileNavBtn.addEventListener("click", navOpen);
+
+const aboutTabSwitch = function (e) {
+  const current = e.target;
+  const tabName = e.target.getAttribute("data");
+
+  if (current.classList.contains("tab")) {
+    // Remove active class from all tabs and contents
+    tabLinks.forEach((tab) => tab.classList.remove("active-tab"));
+    tabContents.forEach((content) => content.classList.remove("active-cont"));
+
+    // Add active class to the clicked tab and corresponding content
+
+    current.classList.add("active-tab");
+    document.getElementById(tabName).classList.add("active-cont");
+  }
+};
+
+// Event handlers
+
+tabContainer.addEventListener("click", aboutTabSwitch);
+mobileNavBtn.addEventListener("click", mobileNavOpen);
